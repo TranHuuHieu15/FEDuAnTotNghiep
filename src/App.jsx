6b66b8deeaaadbfb044449bea061b3f62cbe6693
TranHuuHieu <hieutranhuu1405@gmail.com>
2023-10-08 16:27:13 +0700

    lam xong selectbox

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import './App.css'
 // import ContactPage from './pages/ContactPage'
 import Button from './components/button/Button'
 import googleIcon from "./assets/images/googleIcon.svg"
+import Selectbox from './components/selectbox/Selectbox'
 
 function App() {
 
   return (
     <>
       <Button className={"flex items-center gap-3 rounded-full m-2"} outline={"outlined"} color='blue-gray' src={googleIcon} size={"lg"} nameButton='Hieu dep trai siu cap pro'></Button>
+      <Selectbox className={"flex w-72 flex-col gap-6"} nameSelect={"Problem?"} option={"Chưa web chưa đủ hiểu năng"} color={"blue"}></Selectbox>
       {/* <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="/contact" element={<ContactPage></ContactPage>}></Route>

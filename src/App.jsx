2e4bfa7f235a4b46fdf81b384eb88721a225a007
TranHuuHieu <hieutranhuu1405@gmail.com>
2023-10-09 19:54:30 +0700

    update button

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,11 @@ function App() {
 
   return (
     <>
-      <Button className={"flex items-center gap-3 rounded-full m-2"} outline={"outlined"} color='blue-gray' src={googleIcon} size={"lg"} nameButton='Hieu dep trai siu cap pro'></Button>
+      <Button className={"flex items-center gap-3 rounded-full"} outline={"outlined"} color='blue-gray'>
+        <img src={googleIcon} alt="" className={"w-6 h-6"} />
+        Hieu dep trai siu cap pro
+      </Button>
+      <Button color='red' outline={""}>Hiếu Trần</Button>
       {/* <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="/contact" element={<ContactPage></ContactPage>}></Route>

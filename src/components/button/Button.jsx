0417d47e08872cecfaecff852c2030e3febb24fa
TranHuuHieu <hieutranhuu1405@gmail.com>
2023-10-09 19:46:46 +0700

    button component update

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,19 +1,17 @@
 import propTypes from "prop-types";
 import { Button as ButtonTailWind } from "@material-tailwind/react";
-const Button = ({ size, color = "", className, nameButton = "Button", src, outline }) => {
-    return (
-        <ButtonTailWind variant={outline} className={className} size={size} color={color}>
-            <img src={src} alt={nameButton} className="h-6 w-6" />
-            {nameButton}
-        </ButtonTailWind>
-    );
+const Button = ({ color = "", className = "w-[100px]", children, outline }) => {
+  return (
+    <ButtonTailWind variant={outline} className={className} color={color}>
+      {children}
+    </ButtonTailWind>
+  );
 };
 Button.propTypes = {
-    size: propTypes.string,
-    color: propTypes.string,
-    className: propTypes.string,
-    nameButton: propTypes.string,
-    src: propTypes.string,
-    outline: propTypes.string
+  color: propTypes.string,
+  className: propTypes.string,
+  children: propTypes.element,
+  src: propTypes.string,
+  outline: propTypes.string
 };
 export default Button;

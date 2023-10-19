2dc8739a7e8da4229d3a10cd2c65315177075dce
TranHuuHieu <hieutranhuu1405@gmail.com>
2023-10-19 22:34:47 +0700

    H update component select

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -1,20 +1,43 @@
-import { Select as Selectbox } from "@material-tailwind/react";
-import propTypes from "prop-types";
+// import { Select as Selectbox } from "@material-tailwind/react";
+// import propTypes from "prop-types";
 
-const Select = ({ label, children, className = "w-[200px]", color }) => {
+// const Select = ({ label, children, className = "w-[200px]", color }) => {
+//   return (
+//     <div className={className}>
+//       <Selectbox color={color} label={label}>
+//         {children}
+//       </Selectbox>
+//     </div>
+//   );
+// };
+
+// Select.propTypes = {
+//   label: propTypes.string,
+//   children: propTypes.any,
+//   className: propTypes.string,
+//   color: propTypes.string,
+// };
+// export default Select;
+import PropTypes from 'prop-types';
+
+const Select = ({ className, title, color, children }) => {
+  const selectClasses = `border rounded px-3 py-2 ${color} ${className}`;
+
   return (
-    <div className={className}>
-      <Selectbox color={color} label={label}>
+    <div>
+      {title && <label className="block text-sm font-medium text-gray-700">{title}</label>}
+      <select className={selectClasses}>
         {children}
-      </Selectbox>
+      </select>
     </div>
   );
 };
 
 Select.propTypes = {
-  label: propTypes.string,
-  children: propTypes.any,
-  className: propTypes.string,
-  color: propTypes.string,
+  className: PropTypes.string,
+  title: PropTypes.string,
+  color: PropTypes.string,
+  children: PropTypes.node.isRequired,
 };
+
 export default Select;

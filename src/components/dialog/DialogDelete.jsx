a3e634aa3af44fca5b2c8ba48aad4db31202a071
TranHuuHieu <hieutranhuu1405@gmail.com>
2023-11-05 14:03:13 +0700

    css brand

--- src/components/dialog/DialogDelete.jsx
+++ src/components/dialog/DialogDelete.jsx
@@ -14,10 +14,10 @@ const DialogDelete = ({ title, show, confirm, cancel }) => {
         <DialogHeader>{`Delete this ${title}`}</DialogHeader>
         <DialogBody>{`Do you want to delete this ${title}`}</DialogBody>
         <DialogFooter>
-          <Button variant="text" color="red" onClick={cancel} className="mr-1">
+          <Button color="red" onClick={cancel} className="mr-1">
             <span>Cancel</span>
           </Button>
-          <Button variant="gradient" color="green" onClick={confirm}>
+          <Button color="green" onClick={confirm}>
             <span>Confirm</span>
           </Button>
         </DialogFooter>

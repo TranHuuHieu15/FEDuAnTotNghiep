8894cd586e27424d19dba0d095097b3a3b518407
ASUS <thanhle2082003@gmail.com>
2023-11-11 16:03:41 +0700

    SelectBox component change

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -1,12 +1,24 @@
 import PropTypes from "prop-types";
+import { useController } from "react-hook-form";
 
-const Select = ({ className, title, children, className2 }) => {
+const Select = ({ className, title, options, className2, control, name }) => {
   const selectClasses = `border rounded-md ${className}`;
 
+  const { field } = useController({
+    control,
+    name,
+    defaultValue: "",
+  });
   return (
     <>
       {title && <label className={className2}>{title}</label>}
-      <select className={selectClasses}>{children}</select>
+      <select className={selectClasses} {...field}>
+        {options.map((item) => (
+          <option value={item.name} key={item.id}>
+            {item.name}
+          </option>
+        ))}
+      </select>
     </>
   );
 };
@@ -14,8 +26,10 @@ const Select = ({ className, title, children, className2 }) => {
 Select.propTypes = {
   className: PropTypes.string,
   className2: PropTypes.string,
+  control: PropTypes.any.isRequired,
+  name: PropTypes.string,
   title: PropTypes.string,
-  children: PropTypes.any,
+  options: PropTypes.array,
 };
 
 export default Select;

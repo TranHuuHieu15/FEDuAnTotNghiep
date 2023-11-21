1ed9a81e0ae9be5fdad52e126cafe67429eaa968
ASUS <thanhle2082003@gmail.com>
2023-11-21 14:28:32 +0700

    Cart Commit

---
+++ src/redux/features/cartSlice.jsx
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userInfo: [],
+  products: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = state.products.find((item) => item.id === action.payload.id);
+      if (item) {
+        item.quantity += action.payload.quantity;
+      } else {
+        state.products.push(action.payload);
+      }
+    },
+    increaseQuantity: (state, action) => {
+      const item = state.products.find((item) => item.id === action.payload.id);
+      if (item) {
+        item.quantity++;
+      }
+    },
+    decreaseQuantity: (state, action) => {
+      const item = state.products.find((item) => item.id === action.payload.id);
+      if (item && item.quantity > 1) {
+        item.quantity--;
+      }
+    },
+    deleteItem: (state, action) => {
+      state.products = state.products.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+    resetCart: (state) => {
+      state.products = [];
+    },
+  },
+});
+
+export const {
+  addToCart,
+  increaseQuantity,
+  drecreaseQuantity,
+  deleteItem,
+  resetCart,
+} = cartSlice.actions;
+export default cartSlice.reducer;

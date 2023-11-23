d43bd10c6d7df928e11482ce7424b02893349c5a
ASUS <thanhle2082003@gmail.com>
2023-11-23 14:20:50 +0700

    ProductDetail GetData

--- src/components/color/Color.jsx
+++ src/components/color/Color.jsx
@@ -1,23 +1,24 @@
 import PropTypes from "prop-types";
+import { AiOutlineCheck } from "react-icons/ai";
 
-const Color = ({ color }) => {
-  const classNames = `w-8 h-8 border-none rounded-full outline-none cursor-pointer hover:opacity-100`;
-  console.log(
-    "Dynamic classes:",
-    color.map((item) => `bg-[${item.code}]`)
-  );
+const Color = ({ color = [], onColorChange, selectedColor }) => {
+  const classNames = `w-8 h-8 border-none rounded-full outline-none cursor-pointer hover:opacity-100 relative`;
+  const uniqueColors = new Set(color.map((item) => item.colorId));
   return (
     <>
-      <div className="flex gap-4">
-        {color &&
-          color.length > 0 &&
-          color.map((item) => (
-            <span
-              key={item.id}
-              className={`${classNames}`}
-              style={{ backgroundColor: item.code }}
-            ></span>
-          ))}
+      <div className="flex items-center justify-center gap-4">
+        {[...uniqueColors].map((uniqueColor) => (
+          <span
+            key={uniqueColor}
+            className={`${classNames}`}
+            style={{ backgroundColor: uniqueColor }}
+            onClick={() => onColorChange(uniqueColor)}
+          >
+            {selectedColor === uniqueColor && (
+              <AiOutlineCheck className="absolute bottom-0 right-0 text-white transform -translate-x-1/2 -translate-y-1/2" />
+            )}
+          </span>
+        ))}
       </div>
     </>
   );
@@ -25,6 +26,8 @@ const Color = ({ color }) => {
 
 Color.propTypes = {
   color: PropTypes.array.isRequired,
+  selectedColor: PropTypes.string,
+  onColorChange: PropTypes.func.isRequired,
 };
 
 export default Color;

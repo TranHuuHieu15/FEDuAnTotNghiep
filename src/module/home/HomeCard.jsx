bbd4fdb44dc6b411c61d1099745b7f426f58b9b9
ASUS <thanhle2082003@gmail.com>
2023-12-09 16:42:16 +0700

    HomeCard update

--- src/module/home/HomeCard.jsx
+++ src/module/home/HomeCard.jsx
@@ -1,48 +1,30 @@
+import { useState } from "react";
 import ProductCard from "../../components/card/ProductCard";
 import Heading from "../../components/heading/Heading";
-
-const productData = [
-  {
-    id: 1,
-    name: "Airpod Pro 15",
-    img: "https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80",
-    price: 100,
-    discount: 5,
-    rating: 4,
-  },
-  {
-    id: 2,
-    name: "Airpod Pro 15",
-    img: "https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80",
-    price: 100,
-    discount: 5,
-    rating: 5,
-  },
-  {
-    id: 3,
-    name: "Airpod Pro 15",
-    img: "https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80",
-    price: 100,
-    discount: 5,
-    rating: 2,
-  },
-  {
-    id: 4,
-    name: "Airpod Pro 15",
-    img: "https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80",
-    price: 100,
-    discount: 5,
-    rating: 3,
-  },
-];
+import { useEffect } from "react";
+import axios from "../../config/axios";
 
 const HomeCard = () => {
+  const [topProduct, setTopProduct] = useState([]);
+  const top = 4;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/statistic/top-product?top=${top}`);
+        console.log(response);
+        setTopProduct(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
-      <Heading className="text-center">Popular this week</Heading>
+      <Heading className="text-center">The Highest-Priced Products.</Heading>
       <div className="flex items-center justify-center gap-5">
-        {productData.length > 0 &&
-          productData.map((item) => (
+        {topProduct?.length > 0 &&
+          topProduct.map((item) => (
             <ProductCard
               className="w-96"
               key={item.id}

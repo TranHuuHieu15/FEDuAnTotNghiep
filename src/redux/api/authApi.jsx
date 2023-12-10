271b2359de24935867b511e697dd4357eff3529e
ASUS <thanhle2082003@gmail.com>
2023-12-10 14:42:57 +0700

    Thanh commit

--- src/redux/api/authApi.jsx
+++ src/redux/api/authApi.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApiSlice",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080",
+    baseUrl: "http://localhost:8080/api/ttf/",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.userToken;
       if (token) {
@@ -15,30 +15,18 @@ export const authApi = createApi({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: data?.token
-          ? `/api/ttf/auth/login?token=${data.token}`
-          : "/api/ttf/auth/login",
+        url: data?.token ? `auth/login?token=${data.token}` : "auth/login",
         method: "POST",
         body: data,
       }),
     }),
     register: builder.mutation({
       query: (data) => ({
-        url: "/api/ttf/auth/register",
+        url: "auth/register",
         method: "POST",
         body: data,
       }),
     }),
-    Oauth2Google: builder.mutation({
-      query: () => ({
-        url: "/google",
-        method: "GET",
-      }),
-    }),
   }),
 });
-export const {
-  useLoginMutation,
-  useRegisterMutation,
-  useOauth2GoogleMutation,
-} = authApi;
+export const { useLoginMutation, useRegisterMutation } = authApi;

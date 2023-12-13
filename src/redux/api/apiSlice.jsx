d0ec64496af0ce91e0a490cc52346967bdb21f79
ASUS <thanhle2082003@gmail.com>
2023-12-13 13:08:19 +0700

    Thanh commit

--- src/redux/api/apiSlice.jsx
+++ src/redux/api/apiSlice.jsx
@@ -1,9 +1,8 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { loginSuccess, logout } from "../features/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:8080/api/ttf",
-  credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.userToken;
     if (token) {

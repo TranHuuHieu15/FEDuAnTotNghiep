75b951b4de6b77282dfcafce2eb065d7e7148a3c
phamsonghiep <phamsonghiepb3k46@gmail.com>
2023-12-15 18:33:15 +0700

    đấy chức năng delete Product and phân trang product

--- src/module/product/ProductManage.jsx
+++ src/module/product/ProductManage.jsx
@@ -1,30 +1,97 @@
 import React, { useEffect, useState } from "react";
 import Button from "../../components/button/Button";
-
+import DialogDelete from "../../components/dialog/DialogDelete.jsx";
 import axios from "../../config/axios.js";
-// import { toast } from "react-toastify";
+import { toast } from "react-toastify";
 import { CiEdit } from "react-icons/ci";
 import { BsTrash3 } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import { selectCurrentToken } from "../../redux/features/authSlice.jsx";
+import { useSelector } from "react-redux";
+import Pagination from "../../components/pagination/Pagination.jsx";
 
 const ProductManage = () => {
+  const token = useSelector(selectCurrentToken);
   const [productData, setProductData] = useState([]);
   const navigate = useNavigate();
+  const [currentPage, setCurrentPage] = useState(0); // Thêm state trang hiện tại
+  const [totalPages, setTotalPages] = useState(0); // Thêm state tổng số trang
+  const [showDialog, setShowDialog] = useState({
+    show: false,
+    id: null,
+  });
+
 
   //Call api
-  const fetchData = async () => {
-    try {
-      const response = await axios.get("/product");
-      setProductData(response.data);
-      // console.log(response.data);
-    } catch (error) {
-      console.log(error);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/product?page=${currentPage}`);
+        setProductData(response.data || response.content);
+        const totalPages = Math.ceil(response["all-item"] / response.size);
+        setTotalPages(totalPages); // Cập nhật tổng số trang
+      } catch (error) {
+        console.log(error);
+      }
     }
+    fetchData();
+  }, [currentPage]);
+
+  const handleChangePage = (page) => {
+    setCurrentPage(page);
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const handleCloseDialog = () => {
+    setShowDialog({
+      show: false,
+      id: null,
+    });
+  };
+
+  const handleDeleteTrue = (id) => {
+    setShowDialog({
+      show: true,
+      id: id,
+    });
+  };
+
+  const handleDelete = async () => {
+    try {
+      if (showDialog.show && showDialog.id) {
+        await axios.delete(`/product/delete/${showDialog.id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setProductData(
+          productData.filter((item) => item.id !== showDialog.id)
+        );
+        handleCloseDialog();
+        toast.success("Delete product successfully!", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    } catch (response) {
+      handleCloseDialog();
+      toast.error(response.response.data.message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
 
   return (
     <>
@@ -52,10 +119,10 @@ const ProductManage = () => {
           {productData.length > 0 &&
             productData.map((item) => (
               <tr key={item.id}>
-                <td className="w-9">
+                <td className="w-12">
                   <img src={item.main_image} alt="" />
                 </td>
-                <td className="p-2 font-medium text-gray-800">{item.name}</td>
+                <td className="w-1/4 font-medium text-gray-800">{item.name}</td>
                 <td className="">{item.min_price}</td>
                 <td className="">{item.max_price}</td>
                 <td className="">{item.quantity}</td>
@@ -72,7 +139,7 @@ const ProductManage = () => {
                     </a>
                     <a
                       className="ml-2 p-2 text-2xl  hover:text-blue-500 cursor-pointer"
-                    // onClick={() => handleDeleteTrue(item.id)}
+                      onClick={() => handleDeleteTrue(item.id)}
                     >
                       <BsTrash3 />
                     </a>
@@ -82,6 +149,19 @@ const ProductManage = () => {
             ))}
         </tbody>
       </table>
+      <div className="flex items-center justify-center">
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onChange={handleChangePage}
+        ></Pagination>
+      </div>
+      <DialogDelete
+        show={showDialog.show}
+        title="Product"
+        confirm={handleDelete}
+        cancel={handleCloseDialog}
+      />
     </>
   );
 };
